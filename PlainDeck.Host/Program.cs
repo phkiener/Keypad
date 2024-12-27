837688372abb8a4a7f158d535f158973fb399203
Philipp Kiener <kiener.phil@gmail.com>
2024-12-27 16:18:58 +0100

    Simple read loop

---
+++ PlainDeck.Host/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PlainDeck.Host;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var cancelSource = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, _) => cancelSource.Cancel();
+
+        var device = DeviceHandle.Find();
+        await device.ListenAsync(cancelSource.Token);
+    }
+}
