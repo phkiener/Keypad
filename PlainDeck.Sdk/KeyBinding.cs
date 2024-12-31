2432c08d6016b898bfa4d87f4af4ee37b6a3054f
Philipp Kiener <kiener.phil@gmail.com>
2024-12-31 12:01:15 +0100

    Multiple keys

--- PlainDeck.Sdk/KeyBinding.cs
+++ PlainDeck.Sdk/KeyBinding.cs
@@ -4,13 +4,16 @@ namespace PlainDeck.Sdk;
 
 public static class KeyBinding
 {
-    public static void BindKey(this Device device, DeviceKey key, ConsoleKey emittedKey)
+    public static void BindKey(this Device device, DeviceKey key, params ConsoleKey[] emittedKeys)
     {
         device.OnKeyDown += (_, pressedKey) =>
         {
             if (pressedKey == key)
             {
-                KeyEmitter.KeyDown(emittedKey);
+                foreach (var singleKey in emittedKeys)
+                {
+                    KeyEmitter.KeyDown(singleKey);
+                }
             }
         };
 
@@ -18,7 +21,10 @@ public static class KeyBinding
         {
             if (pressedKey == key)
             {
-                KeyEmitter.KeyUp(emittedKey);
+                foreach (var singleKey in emittedKeys.Reverse())
+                {
+                    KeyEmitter.KeyDown(singleKey);
+                }
             }
         };
     }
